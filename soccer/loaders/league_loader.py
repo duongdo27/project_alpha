f5159af758f2e7f6c711ad452fbae696df0013b1
Duong Do <dohoangduong95@gmail.com>
2017-06-07 23:24:42 -0500

    Add date for match

--- soccer/loaders/league_loader.py
+++ soccer/loaders/league_loader.py
@@ -2,6 +2,7 @@ from soccer.models import League, Match, Team
 import requests
 from lxml import html
 from fuzzywuzzy import fuzz
+from datetime import datetime
 
 ABBREV = {"England": "eng"}
 
@@ -13,18 +14,22 @@ class LeagueLoader(object):
         self.parent = parent
 
         self.league, _ = League.objects.get_or_create(name=self.name, year=self.year, parent=self.parent)
+        Match.objects.filter(league=self.league).delete()
         self.teams = []
+        self.matches = []
 
         self.round = 0
         self.cache_lookup = {}
-        self.date = None
+        self.raw_date = None
 
     def get_raw_text(self):
         url = "http://www.rsssf.com/tablese/{}{}.html".format(ABBREV[self.parent], self.year)
         print url
         res = requests.get(url)
         tree = html.fromstring(res.text)
-        return tree.xpath("//pre/text()")[0]
+        for raw_text in tree.xpath("//pre[1]/text()"):
+            if raw_text.strip():
+                return raw_text
 
     def create_team_from_line(self, line):
         if len(line) > 2 and line[1].isdigit() and line[2] == ".":
@@ -36,7 +41,7 @@ class LeagueLoader(object):
         if len(line) > 2 and line[1].isdigit() and line[2] == ".":
             return
         if line.startswith("[") and line.endswith("]"):
-            self.date = line[1:-1]
+            self.raw_date = line[1:-1]
             return
         if '-' in line:
             dash_index = line.index("-")
@@ -47,8 +52,17 @@ class LeagueLoader(object):
                         home_score, away_score = value.split("-")
                         home_team = self.find_team_from_name(" ".join(ls[:index]))
                         away_team = self.find_team_from_name(" ".join(ls[index + 1:]))
-                        Match.objects.get_or_create(league=self.league, home_team=home_team, away_team=away_team,
-                                                    home_score=home_score, away_score=away_score, round=self.round)
+                        match, _ = Match.objects.get_or_create(
+                                league=self.league, home_team=home_team, away_team=away_team,
+                                home_score=home_score, away_score=away_score, round=self.round,
+                                date=self.convert_date(self.raw_date))
+                        self.matches.append(match)
+
+    def convert_date(self, raw_date):
+        the_date = datetime.strptime(raw_date, "%b %d").date()
+        if the_date.month >= 7:
+            return the_date.replace(year=self.year-1)
+        return the_date.replace(year=self.year)
 
     def find_team_from_name(self, name):
         if name in self.cache_lookup:
@@ -77,3 +91,4 @@ class LeagueLoader(object):
     def run(self):
         raw_text = self.get_raw_text()
         self.process_raw_text(raw_text)
+        assert self.matches, "Cannot find any match"
