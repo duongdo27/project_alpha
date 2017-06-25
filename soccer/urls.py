63679e53d8dfd2b0679c39e577421ad4bff3e144
Duong Do <dohoangduong95@gmail.com>
2017-06-25 15:04:24 +0700

    Add basic view and route for soccer app

---
+++ soccer/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^leagues$', views.LeagueListView.as_view(),
+        name='leagues'),
+    url(r'^league_years/(?P<parent>.+)/(?P<name>.+)$', views.LeagueYearListView.as_view(),
+        name='league_years'),
+    url(r'^league/(?P<pk>\d+)$', views.LeagueDetailView.as_view(),
+        name='league_detail'),
+]
--- soccer/views.py
+++ soccer/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import League, Match, Team
+
+
+class LeagueListView(ListView):
+    model = League
+    template_name = 'leagues.html'
+
+    def get_queryset(self):
+        return League.objects.all().values('name', 'parent').distinct()
+
+
+class LeagueYearListView(ListView):
+    model = League
+    template_name = 'league_years.html'
+
+    def get_queryset(self):
+        return League.objects.filter(parent=self.kwargs['parent'],
+                                     name=self.kwargs['name'])
+
+
+class LeagueDetailView(DetailView):
+    model = League
+    template_name = 'league_detail.html'
