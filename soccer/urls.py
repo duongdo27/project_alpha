55864ddaf732e0b6d15adac63a3972139cc2b6fe
Duong Do <dohoangduong95@gmail.com>
2017-06-26 07:27:21 +0700

    Add Boostrap and update views

--- soccer/urls.py
+++ soccer/urls.py
@@ -4,10 +4,13 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^$', views.HomeView.as_view(), name='index'),
     url(r'^leagues$', views.LeagueListView.as_view(),
         name='leagues'),
     url(r'^league_years/(?P<parent>.+)/(?P<name>.+)$', views.LeagueYearListView.as_view(),
         name='league_years'),
     url(r'^league/(?P<pk>\d+)$', views.LeagueDetailView.as_view(),
         name='league_detail'),
+    url(r'^league/(?P<league_id>\d+)/(?P<round>\d+)$', views.RoundDetailView.as_view(),
+        name='round'),
 ]
--- soccer/views.py
+++ soccer/views.py
@@ -1,7 +1,13 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
+from django.db.models import Max
 
 from .models import League, Match, Team
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
 
 
 class LeagueListView(ListView):
@@ -18,9 +24,35 @@ class LeagueYearListView(ListView):
 
     def get_queryset(self):
         return League.objects.filter(parent=self.kwargs['parent'],
-                                     name=self.kwargs['name'])
+                                     name=self.kwargs['name']).order_by('-year')
+
+    def get_context_data(self, **kwargs):
+        context = super(LeagueYearListView, self).get_context_data(**kwargs)
+        context['parent'] = self.kwargs['parent']
+        context['name'] = self.kwargs['name']
+        return context
 
 
 class LeagueDetailView(DetailView):
     model = League
     template_name = 'league_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LeagueDetailView, self).get_context_data(**kwargs)
+        max_round = Match.objects.filter(league=self.object).aggregate(Max('round'))['round__max']
+        context['rounds'] = range(1, max_round+1)
+        return context
+
+
+class RoundDetailView(ListView):
+    model = Match
+    template_name = 'round.html'
+
+    def get_queryset(self):
+        return Match.objects.filter(league_id=self.kwargs['league_id'],
+                                    round=self.kwargs['round'])
+
+    def get_context_data(self, **kwargs):
+        context = super(RoundDetailView, self).get_context_data(**kwargs)
+        context['league_id'] = self.kwargs['league_id']
+        return context
