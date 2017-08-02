a46513b920210ed10c13970b19f61711f70f24f6
Duong Do <dohoangduong95@gmail.com>
2017-08-02 13:37:42 -0500

    Add color converter draft

--- converter/urls.py
+++ converter/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^timestamp$', views.TimestampView.as_view(), name='timestamp'),
+    url(r'^color$', views.ColorView.as_view(), name='color'),
 ]
--- converter/views.py
+++ converter/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class TimestampView(TemplateView):
     template_name = 'timestamp.html'
+
+
+class ColorView(TemplateView):
+    template_name = 'color.html'
